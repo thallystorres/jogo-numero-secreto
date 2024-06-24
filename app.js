68feb29b5f15998286e09c7e4a7e0042f4aaecea
Thallys Torres <thallys.auugusto.torres@gmail.com>
2024-06-23 21:19:59 -0300

    BORA

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 30');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
-
 exibirMensagemInicial();
 
 function verificarChute() {
