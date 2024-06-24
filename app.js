8fa230021dabaad7c181f867b5460e3435eabf40
Thallys Torres <thallys.auugusto.torres@gmail.com>
2024-06-23 21:20:59 -0300

    opa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-
+let oi = '1';
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
